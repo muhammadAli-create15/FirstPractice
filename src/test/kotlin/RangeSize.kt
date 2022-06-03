b2d351e02b979f35afa9fa2e5b40319c3b599adb
Muhammad Ali <72379752+muhammadAli-create15@users.noreply.github.com>
2022-06-03 23:33:03 +0300

    Another example of a for loop

---
+++ src/test/kotlin/RangeSize.kt
@@ -0,0 +1,6 @@
+fun main(){
+    for(i in 10..20) println(i)//printing from a certain range to another
+    for(m in  'P'..'Z') println(m)//printing from a range of characters to another
+    for(n in 0..10 step 2) println(n)//printing while skipping two values to a certain number which is 10
+      for(d in 9 downTo 3) println(d)//printing from 9 downwards to 3
+}
