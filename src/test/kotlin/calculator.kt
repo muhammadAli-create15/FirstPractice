3a78ec77058f36d87d418419d419b67dea2ff7a6
Muhammad Ali <72379752+muhammadAli-create15@users.noreply.github.com>
2022-06-03 18:10:13 +0300

    Some operations using kotlin language

---
+++ src/test/kotlin/calculator.kt
@@ -0,0 +1,18 @@
+fun main(){
+val x = 15
+    val y = 20
+    val z = x *y
+    val n = x+ y
+    val p=x - y
+    val  q = x % y
+    val d= x/y
+    println("$z")//product
+    println("$n")//sum
+    println("$p")//subtraction
+    println("$q")//modulus
+  println("$d")//division
+
+
+
+
+}
